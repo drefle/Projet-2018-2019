b4e6f2976d4d009e886a9064058fd6881b22fca7
drefle <48018578+drefle@users.noreply.github.com>
2019-03-20 11:22:52 +0100

    Add files via upload
    Exécutable du jeu, doit être présent dans le même dossier que les autres fichiers pour être exécuté (nécessaire de demandé quoi faire lors de l'execution du fichier (gestionnaire de fichier -> préférences -> comportement -> cocher demander quoi faire) et autorisé l'exécution du fichier comme un programme dans ses propriétés)

---
+++ The Binding Of Isaac Remastered.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+resize -s $Xaxis $Yaxis
+make clean
+make
+./jeu
+make clean
+exit 0
